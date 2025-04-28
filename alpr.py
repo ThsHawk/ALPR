354f2d3593857bd341f8837d4a8baed57a00aae3
Thales Alves <thalesadsilva@outlook.com>
2025-04-28 17:45:49 -0300

    dev#no plot

--- alpr.py
+++ alpr.py
@@ -27,17 +27,17 @@ path = kagglehub.dataset_download("andrewmvd/car-plate-detection")
 dataSetPath = path + "/images/"
 
 img = cv2.imread(dataSetPath + "Cars0.png") #read image
-plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-plt.title('Original Image')
-plt.show()
+#plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+#plt.title('Original Image')
+#plt.show()
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert image to gray
 bfilter = cv2.bilateralFilter(gray, 11, 17, 17) #Noise reduction
-plt.imshow(cv2.cvtColor(bfilter, cv2.COLOR_BGR2RGB)) #show processed image
-plt.title('Processed Image')
+#plt.imshow(cv2.cvtColor(bfilter, cv2.COLOR_BGR2RGB)) #show processed image
+#plt.title('Processed Image')
 
 edged = cv2.Canny(bfilter, 30, 200) #Edge detection
-plt.imshow(cv2.cvtColor(edged, cv2.COLOR_BGR2RGB))
+#plt.imshow(cv2.cvtColor(edged, cv2.COLOR_BGR2RGB))
 
 keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) #Find contours
 contours = imutils.grab_contours(keypoints) #Grab contours
@@ -51,28 +51,31 @@ for contour in contours:
         location = approx
         break
 
-print("Location: ", location)
+#print("Location: ", location)
 
 mask = np.zeros(gray.shape, np.uint8) #create blank image with same dimensions as the original image
 new_image = cv2.drawContours(mask, [location], 0,255, -1) #Draw contours on the mask image
 new_image = cv2.bitwise_and(img, img, mask=mask) #Take bitwise AND between the original image and mask image
 
-plt.imshow(cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB)) #show the final image
+#plt.imshow(cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB)) #show the final image
 
 (x,y) = np.where(mask==255) #Find the co-ordinates of the four corners of the document
 (x1, y1) = (np.min(x), np.min(y)) #Find the top left corner
 (x2, y2) = (np.max(x), np.max(y)) #Find the bottom right corner
 cropped_image = gray[x1:x2+1, y1:y2+1] #Crop the image using the co-ordinates
 
-plt.imshow(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB)) #show the cropped image
+#plt.imshow(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB)) #show the cropped image
 
 reader = easyocr.Reader(['en']) #create an easyocr reader object with english as the language
 result = reader.readtext(cropped_image) #read text from the cropped image
-result
+#result
 
 text = result[0][-2] #Extract the text from the result
-font = cv2.FONT_HERSHEY_SIMPLEX #Font style
-res = cv2.putText(img, text=text, org=(approx[0][0][0], approx[1][0][1]+60), fontFace=font, fontScale=1, color=(0,255,0), thickness=2, lineType=cv2.LINE_AA) #put the text on the image
-res = cv2.rectangle(img, tuple(approx[0][0]), tuple(approx[2][0]), (0,255,0),3) #Draw a rectangle around the text
 
-plt.imshow(cv2.cvtColor(res, cv2.COLOR_BGR2RGB)) #show the final image with text
+print(text)
+
+#font = cv2.FONT_HERSHEY_SIMPLEX #Font style
+#res = cv2.putText(img, text=text, org=(approx[0][0][0], approx[1][0][1]+60), fontFace=font, fontScale=1, color=(0,255,0), thickness=2, lineType=cv2.LINE_AA) #put the text on the image
+#res = cv2.rectangle(img, tuple(approx[0][0]), tuple(approx[2][0]), (0,255,0),3) #Draw a rectangle around the text
+
+#plt.imshow(cv2.cvtColor(res, cv2.COLOR_BGR2RGB)) #show the final image with text
